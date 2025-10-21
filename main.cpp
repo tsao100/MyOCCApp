801ed583dd6859565b205bbe0e7b2d6bbf86d69d
tsao100 <tsao100@ms17.hinet.net>
2025-10-21 23:10:39 +0800

    XCB env check and set

--- main.cpp
+++ main.cpp
@@ -3,6 +3,14 @@
 
 int main(int argc, char *argv[])
 {
+    // Force Qt to use XCB before QApplication is created
+    const char* session = std::getenv("XDG_SESSION_TYPE");
+
+    if (session && std::strcmp(session, "wayland") == 0) {
+        qDebug("Detected Wayland session → forcing xcb");
+        qputenv("QT_QPA_PLATFORM", QByteArray("xcb"));
+    }
+
     QApplication app(argc, argv);
     MainWindow w;
     w.show();
